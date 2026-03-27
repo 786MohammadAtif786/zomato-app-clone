9564c476eae4572b315317345edcdb3b9ebef045
Mohammad Atif <mohd.atif81999@gmail.com>
2026-03-27 14:33:10 +0530

    added route

--- services/auth/dist/index.js
+++ services/auth/dist/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRoute from "./router/auth.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use("/api/auth", authRoute);
 app.listen(PORT, () => {
     console.log(`Auth service is running on ${PORT}`);
     connectDB();
