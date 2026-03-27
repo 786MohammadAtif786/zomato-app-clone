18ff9cd07919600aa9410ae75b425955838839bd
Mohammad Atif <mohd.atif81999@gmail.com>
2026-03-27 21:18:37 +0530

    login sucessfully with jwt

--- services/auth/dist/controllers/auth.js
+++ services/auth/dist/controllers/auth.js
@@ -1,3 +1,22 @@
-export const loginUser = async (req, res) => {
-    res.json(req.body);
-};
+import User from "../model/User.js";
+import jwt from "jsonwebtoken";
+import TryCatch from "../middlewares/tryCatch.js";
+export const loginUser = TryCatch(async (req, res) => {
+    const { email, name, picture } = req.body;
+    let user = await User.findOne({ email });
+    if (!user) {
+        user = await User.create({
+            name,
+            email,
+            image: picture
+        });
+    }
+    const token = jwt.sign({ user }, process.env.JWT_SECRECT, {
+        expiresIn: "5d"
+    });
+    res.json({
+        message: "login success",
+        token,
+        user
+    });
+});
