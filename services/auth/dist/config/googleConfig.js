6cd3b5a2e6c70ed76f1969cc73fb68c98abbc3d0
Mohammad Atif <mohd.atif81999@gmail.com>
2026-03-28 22:02:19 +0530

    google apis configure

---
+++ services/auth/dist/config/googleConfig.js
@@ -0,0 +1,6 @@
+import { google } from "googleapis";
+import dotenv from "dotenv";
+dotenv.config();
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOLGE_CLIENT_SECRET = process.env.GOOLGE_CLIENT_SECRET;
+export const oauth2client = new google.auth.OAuth2(GOOGLE_CLIENT_ID, GOOLGE_CLIENT_SECRET, "postmessage");
