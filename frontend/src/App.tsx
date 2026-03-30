173fb6b52e9420a2d252948a1cd06bd821fce81c
Mohammad Atif <mohd.atif81999@gmail.com>
2026-03-30 11:38:18 +0530

    create a protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,21 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import { Toaster } from "react-hot-toast";
+import ProtectedRoutes from "./components/protectedRoutes";
+import PublicRoute from "./components/publicRouter";
 
 function App() {
   return (
    <>
     <BrowserRouter>
       <Routes>
+        <Route element={<PublicRoute />} >
+             <Route path="/login" element={<Login />} />
+        </Route>
+        <Route element={<ProtectedRoutes />} >
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        </Route>
+       
       </Routes>
       <Toaster />
     </BrowserRouter>
---
+++ frontend/src/components/protectedRoutes.tsx
@@ -0,0 +1,23 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useAppData } from "../context/AppContext"
+
+const ProtectedRoutes = () => {
+    const {isAuth, user, loading} = useAppData();
+
+    if(loading) return null;
+
+    const location = useLocation();
+
+    if(!isAuth) {
+        return <Navigate to= {'/login'} replace />
+    }
+    if(user?.role === null && location.pathname !== "/select-role") {
+        return <Navigate to={'/select-role'} replace /> 
+    }
+     if(user?.role !== null && location.pathname === "/select-role") {
+        return <Navigate to={'/'} replace /> 
+    }
+    return <Outlet />
+}
+
+export default ProtectedRoutes
