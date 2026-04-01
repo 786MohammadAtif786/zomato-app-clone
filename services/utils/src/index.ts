79936975269f5d10376b1a01a38a6b048b9c91cd
Mohammad Atif <mohd.atif81999@gmail.com>
2026-04-01 11:29:45 +0530

    setup cloudinary

--- services/utils/src/index.ts
+++ services/utils/src/index.ts
@@ -1,10 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import cloudinary from "cloudinary";
 
 dotenv.config();
 
 
 const app = express();
+
+const { CLOUD_NAME, CLOUD_API_KEY, CLOUD_SECRET_KEY } = process.env;
+
+if (!CLOUD_NAME || !CLOUD_API_KEY || !CLOUD_SECRET_KEY) {
+  throw new Error("Missing Cloudinary environment variables");
+}
+
+cloudinary.v2.config({
+  cloud_name: CLOUD_NAME,
+  api_key: CLOUD_API_KEY,
+  api_secret: CLOUD_SECRET_KEY,
+});
+
 
 const PORT = process.env.PORT || 3002;
 
