d71994b6002b8f375bfc44215acfd8d849c4efa0
Mohammad Atif <mohd.atif81999@gmail.com>
2026-04-03 23:45:52 +0530

    cart: qunatity increase

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 import { useAppData } from "../context/AppContext"
 import { useState } from "react";
 import type { IRestaurant } from "../types";
+import axios from "axios";
+import { restaurantService } from "../main";
 
 function Cart() {
   const{cart, subTotal, quauntity, fetchCart} = useAppData();
@@ -24,6 +26,27 @@ function Cart() {
   const platfromFee = 7;
 
   const grandTotal = subTotal + deliveryFee + platfromFee;
+
+   const increaseQty = async (itemId: string) => {
+    try {
+      setLoadingItemId(itemId);
+      await axios.put(
+        `${restaurantService}/api/cart/inc`,
+        { itemId },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+
+      await fetchCart();
+    } catch (error) {
+      toast.error("something went wrong");
+    } finally {
+      setLoadingItemId(null);
+    }
+  };
 
   return (
     <div>MealGo Cart</div>
