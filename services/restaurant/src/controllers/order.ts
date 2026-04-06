10e848ecd9106c15afe7efad7f6fe0ebb399cb9c
Mohammad Atif <mohd.atif81999@gmail.com>
2026-04-06 18:05:25 +0530

    feat: create an order api add a address

---
+++ services/restaurant/src/controllers/order.ts
@@ -0,0 +1,32 @@
+import { AuthenticatedRequest } from "../middleware/isAuth.js";
+import TryCatch from "../middleware/tryCatch.js";
+import Address from "../model/Address.js";
+
+
+export const createOrder = TryCatch(async(req: AuthenticatedRequest, res) => {
+    const user = req.user;
+    if(!user) {
+        return res.status(401).json({
+            message: "Unauthorized"
+        });
+    }
+    const { paymentMethod, addressId } = req.body;
+
+    if(!addressId) {
+        return res.status(400).json({
+            message: "Address is required"
+        })
+    }
+
+    const address = await Address.findOne({
+        _id: addressId,
+        userId: user._id,
+    })
+    if(!address) {
+        return res.status(404).json({
+            message: "Address Not found"
+        })
+    }
+    
+})
+
