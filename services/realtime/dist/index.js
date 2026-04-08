2a982f98ce6af7c6944d755ded553b4102d98e40
Mohammad Atif <mohd.atif81999@gmail.com>
2026-04-08 15:09:37 +0530

    create a socket.io connection

--- services/realtime/dist/index.js
+++ services/realtime/dist/index.js
@@ -2,19 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import http from "http";
-
-
 dotenv.config();
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
-
 const server = http.createServer(app);
-
-
 server.listen(process.env.PORT, () => {
-  console.log(`Realtime service is running port ${process.env.PORT}`);
+    console.log(`Realtime service is running port ${process.env.PORT}`);
 });
