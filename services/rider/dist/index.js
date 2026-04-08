42b5bb8f0946e4dc492b65d05900248af7058406
Mohammad Atif <mohd.atif81999@gmail.com>
2026-04-08 23:34:00 +0530

    connected to mongodb and add cors

--- services/rider/dist/index.js
+++ services/rider/dist/index.js
@@ -1,17 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors"
+import cors from "cors";
 import connectDB from "./config/db.js";
-
 dotenv.config();
-
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
-
-connectDB()
-
+connectDB();
 app.listen(process.env.PORT, () => {
     console.log(`Rider service is running on port ${process.env.PORT}`);
-    
-})
+});
