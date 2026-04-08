3df5029d7da2bed9454fc7dc73e4e952bb05bad1
Mohammad Atif <mohd.atif81999@gmail.com>
2026-04-08 16:05:21 +0530

    api: update order status

--- services/restaurant/dist/routes/order.js
+++ services/restaurant/dist/routes/order.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { isAuth, isSeller } from "../middleware/isAuth.js";
-import { createOrder, fetchOrderForPayment, fetchRestaurantOrders } from "../controllers/order.js";
+import { createOrder, fetchOrderForPayment, fetchRestaurantOrders, updateOrderStatus } from "../controllers/order.js";
 const router = express.Router();
 router.post("/new", isAuth, createOrder);
 router.get("/payment/:id", fetchOrderForPayment);
 router.get("/restaurant/:restaurantId", isAuth, isSeller, fetchRestaurantOrders);
+router.put("/:orderId", isAuth, isSeller, updateOrderStatus);
 export default router;
--- services/restaurant/src/routes/order.ts
+++ services/restaurant/src/routes/order.ts
@@ -3,7 +3,8 @@ import { isAuth, isSeller } from "../middleware/isAuth.js"
 import {
   createOrder,
   fetchOrderForPayment,
-  fetchRestaurantOrders
+  fetchRestaurantOrders,
+  updateOrderStatus
 
 } from "../controllers/order.js";
 
@@ -18,5 +19,6 @@ router.get(
   isSeller,
   fetchRestaurantOrders
 );
+router.put("/:orderId", isAuth, isSeller, updateOrderStatus);
 
 export default router;
