7c86026fc8e03b65be14d92b79756b56c2b2c977
Mohammad Atif <mohd.atif81999@gmail.com>
2026-04-08 15:01:12 +0530

    create real time services

--- services/utils/src/controllers/payment.ts
+++ services/utils/src/controllers/payment.ts
@@ -59,3 +59,71 @@ export const verifyRazorpayPayment = async (req: Request, res: Response) => {
     message: "Payment verified successfully",
   });
 };
+
+
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// import Stripe from "stripe";
+
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+
+// const stripeSecret = process.env.STRIPE_SECRET_KEY;
+
+// if (!stripeSecret) {
+//   throw new Error("STRIPE_SECRET_KEY missing");
+// }
+
+// const stripe = new Stripe(stripeSecret, {
+//   apiVersion: "2024-04-10",
+// });
+
+// export const payWithStripe = async (req: Request, res: Response) => {
+//   try {
+//     const { orderId } = req.body;
+
+//     const { data } = await axios.get(
+//       `${process.env.RESTAURANT_SERVICE}/api/order/payment/${orderId}`,
+//       {
+//         headers: {
+//           "x-internal-key": process.env.INTERNAL_SERVICE_KEY,
+//         },
+//       }
+//     );
+
+//     const session = await stripe.checkout.sessions.create({
+//       payment_method_types: ["card"],
+//       mode: "payment",
+
+//       line_items: [
+//         {
+//           price_data: {
+//             currency: "inr",
+//             product_data: {
+//               name: "Tomato food order",
+//             },
+//             unit_amount: data.amount * 100,
+//           },
+//           quantity: 1,
+//         },
+//       ],
+
+//       metadata: {
+//         orderId,
+//       },
+
+//       success_url: `${process.env.FRONTEND_URL}/ordersuccess?session_id={CHECKOUT_SESSION_ID}`,
+//       cancel_url: `${process.env.FRONTEND_URL}/checkout`,
+//     });
+
+//     res.json({
+//       url: session.url,
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       message: "stripe payment failed",
+//     });
+//   }
+// };
