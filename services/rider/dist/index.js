dc558045516a158bab511f1674d18dedfbca405a
Mohammad Atif <mohd.atif81999@gmail.com>
2026-04-09 00:30:44 +0530

    api: create rider api

--- services/rider/dist/index.js
+++ services/rider/dist/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import connectDB from "./config/db.js";
+import cors from "cors";
+import riderRoutes from "./routes/rider.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-connectDB();
+app.use("/api/rider", riderRoutes);
 app.listen(process.env.PORT, () => {
     console.log(`Rider service is running on port ${process.env.PORT}`);
+    connectDB();
 });
--- services/rider/src/index.ts
+++ services/rider/src/index.ts
@@ -1,17 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors"
 import connectDB from "./config/db.js";
+import cors from "cors";
+import riderRoutes from "./routes/rider.js";
+
 
 dotenv.config();
 
+
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
-connectDB()
+app.use("/api/rider", riderRoutes);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Rider service is running on port ${process.env.PORT}`);
-    
-})
+  console.log(`Rider service is running on port ${process.env.PORT}`);
+  connectDB();
+});
